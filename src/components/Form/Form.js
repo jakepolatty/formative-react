d5f700a96933b5b468658031472ae9b96f71c1bb
Jake Polatty <jakepolatty@gmail.com>
2019-06-19 10:17:48 -0700

    Passing form data into the fields on load

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -20,7 +20,6 @@ export default function Form({schema, uiSchema, externalData}) {
 
   // When new form data is passed in, reset the state object
   useEffect(() => {
-    console.log(externalData)
     setFormData(externalData)
   }, [externalData]);
 
@@ -33,7 +32,12 @@ export default function Form({schema, uiSchema, externalData}) {
             <div id={fields.id} key={fields.id}>
               {fields.label !== undefined &&
                 <h2>{fields.label}</h2>}
-              {fields.items.map((field) => {
+              {fields.items.map((field, i) => {
+                // If initial values have been passed in with the form data, overwrite the field object
+                if (formData[fields.id] !== undefined &&
+                  Array.isArray(formData[fields.id]) && i < formData[fields.id].length) {
+                  field.defaultValue = formData[fields.id][i];
+                }
                 return generateForm(field);
               })}
               {fields.description !== undefined &&
@@ -44,11 +48,19 @@ export default function Form({schema, uiSchema, externalData}) {
           // A generic item format has been provided, so compute the number of elements that
           // should be rendered
           let listLength = 1;
+          let valueCount = 0;
           let defaultsCount = 0;
 
+          // If the list has a min length set that to the current list length
           if (fields.minLength !== undefined && fields.minLength > 0) {
             listLength = fields.minLength;
           }
+          // If more values have been passed in with the form data than the min length, update the length 
+          if (formData[fields.id] !== undefined && Array.isArray(formData[fields.id])) {
+            valueCount = formData[fields.id].length;
+            listLength = Math.max(listLength, valueCount);
+          }
+          // If there are more default values than the current list length, 
           if (fields.defaultValue !== undefined && fields.defaultValue.length > 0) {
             defaultsCount = fields.defaultValue.length;
             listLength = Math.max(listLength, defaultsCount);
@@ -59,10 +71,14 @@ export default function Form({schema, uiSchema, externalData}) {
               {fields.label !== undefined &&
                 <h2>{fields.label}</h2>}
               {[...Array(listLength)].map((_, i) => {
-                // For each element within the default list link, check whether it has an initial value
-                if (i < defaultsCount) {
+                // For each element within the computed list length, check whether it has an initial value
+                if (i < valueCount) {
                   let format = fields.inputFormat;
-                  format.initialValue = fields.defaultValue[i];
+                  format.defaultValue = formData[fields.id][i];
+                  return generateForm(format);
+                } else if (i < defaultsCount) {
+                  let format = fields.inputFormat;
+                  format.defaultValue = fields.defaultValue[i];
                   return generateForm(format);
                 } else {
                   return generateForm(fields.itemFormat);
@@ -78,8 +94,17 @@ export default function Form({schema, uiSchema, externalData}) {
         if (Field !== undefined) {
           // At this point the fields argument is at the level of a single field that can be rendered
           const {type, id, defaultValue, ...rest} = fields;
+          
+          // If the form data contains this field, overwrite the default value
+          // Otherwise pass in the default if there is one
+          let initialValue;
+          if (formData[id] !== undefined) {
+            initialValue = formData[id];
+          } else if (defaultValue !== undefined) {
+            initialValue = defaultValue;
+          }
 
-          return (<Field id={id} key={id} {...rest}/>);
+          return (<Field id={id} key={id} initialValue={initialValue} {...rest}/>);
         } else {
           // In the case where no matching field exists, return null
           return null;
