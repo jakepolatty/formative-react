ebe397583fd06fd37a6681df6c94607ddfb1a4a9
Jake Polatty <jakepolatty@gmail.com>
2019-06-26 07:30:37 -0700

    Added single element save button that displays on field update

--- src/components/inputs/Input.js
+++ src/components/inputs/Input.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
-import reactInputMap from '../../utils/reactInputMap.js';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCheck} from '@fortawesome/free-solid-svg-icons';
 
 /**
  * All custom input fields must accept the following props:
@@ -9,17 +12,43 @@ import reactInputMap from '../../utils/reactInputMap.js';
  * - onUpdate: A callback that handles updates to the field value
 **/
 export default function Input({Type, id, initialValue, label, description, onUpdate, onSave, ...rest}) {
+  const [updated, setUpdated] = useState(false);
+
+  const handleUpdate = (newValue) => {
+    setUpdated(true);
+    onUpdate(newValue);
+  }
+
+  const handleSave = () => {
+    setUpdated(false);
+    if (onSave !== undefined) {
+      onSave();
+    }
+  }
+
   return(
     <Form.Group id={id + "-group"}>
       {label !== undefined &&
         <Form.Label id={id + "-label"}>{label}</Form.Label>}
-      <Type
-        id={id}
-        name={id}
-        initialValue={initialValue}
-        onUpdate={onUpdate}
-        {...rest}
-      />
+      <InputGroup>
+        <Type
+          id={id}
+          name={id}
+          initialValue={initialValue}
+          onUpdate={onUpdate !== undefined ?
+            (newValue) => handleUpdate(newValue) : undefined}
+          {...rest}
+        />
+        <InputGroup.Append>
+          <Button
+            variant={updated ? "outline-success" : "outline-light"}
+            disabled={!updated}
+            onClick={() => handleSave()}
+          >
+            <FontAwesomeIcon icon={faCheck}/>
+          </Button>
+        </InputGroup.Append>
+      </InputGroup>
       {description !== undefined &&
         <Form.Text id={id + "-description"}>{description}</Form.Text>}
     </Form.Group>
