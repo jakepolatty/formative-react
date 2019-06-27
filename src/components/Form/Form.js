73c73eda01815b6842f3825b6090f22ddddb8e84
Jake Polatty <jakepolatty@gmail.com>
2019-06-27 12:51:21 -0700

    Refactored the input group generation into separate functions for better organization

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -39,94 +39,9 @@ export default function Form({schema, uiSchema, externalData, schemaID, includeF
     if (fields !== undefined && fields !== null) {
       if (fields.type === "InputGroup") {
         if (fields.items !== undefined && fields.items !== null) {
-          // The specific list of items has been provided
-          let hasVisibleChild = false;
-          let itemGroup = (
-            <div id={fields.id} key={fields.id}>
-              {fields.label !== undefined &&
-                <h2 id={fields.id + "-LABEL"}>{fields.label}</h2>}
-              <div id={fields.id+"-INPUTS"}>
-                {fields.items.map((field, i) => {
-                  // If initial values have been passed in with the form data, overwrite the field object
-                  if (formData[fields.id] !== undefined &&
-                    Array.isArray(formData[fields.id]) && i < formData[fields.id].length) {
-                    field.defaultValue = formData[fields.id][i];
-                  }
-                  // If the input group is of type array, add the array index for update handling
-                  if (fields.groupType === "array") {
-                    field.arrayIndex = i;
-                  }
-                  
-                  // If a child is visible, indicate that the group should be shown
-                  let child = generateForm(field);
-                  if (child !== null) {
-                    hasVisibleChild = true;
-                  }
-
-                  return child;
-                })}
-              </div>
-              {fields.description !== undefined &&
-                <p id={fields.id + "-DESCRIPTION"}>{fields.description}</p>}
-            </div>
-          );
-
-          // Only show the input group if it has a child to display
-          if (hasVisibleChild) {
-            return itemGroup;
-          } else {
-            return null;
-          }
+          return generateInputList(fields);
         } else if (fields.itemFormat !== undefined && fields.itemFormat !== null) {
-          // A generic item format has been provided, so compute the number of elements that
-          // should be rendered
-          let listLength = 1;
-          let valueCount = 0;
-          let defaultsCount = 0;
-
-          // If the list has a min length set that to the current list length
-          if (fields.minItems !== undefined && fields.minItems > 0) {
-            listLength = fields.minItems;
-          }
-          // If more values have been passed in with the form data than the min length, update the length 
-          if (formData[fields.id] !== undefined && Array.isArray(formData[fields.id])) {
-            valueCount = formData[fields.id].length;
-            listLength = Math.max(listLength, valueCount);
-          }
-          // If there are more default values than the current list length, 
-          if (fields.defaultValue !== undefined && fields.defaultValue.length > 0) {
-            defaultsCount = fields.defaultValue.length;
-            listLength = Math.max(listLength, defaultsCount);
-          }
-          
-          if (includeFields.includes(fields.id)) {
-            return (
-              <div id={fields.id} key={fields.id}>
-                {fields.label !== undefined &&
-                  <h2 id={fields.id + "-LABEL"}>{fields.label}</h2>}
-                <div id={fields.id+"-INPUTS"}>
-                  {[...Array(listLength)].map((_, i) => {
-                    // For each element within the computed list length, check whether it has an initial value
-                    let format = fields.itemFormat;
-                    format.arrayIndex = i;
-                    if (i < valueCount) {
-                      format.defaultValue = formData[fields.id][i];
-                    } else if (i < defaultsCount) {
-                      format.defaultValue = fields.defaultValue[i];
-                    } else {
-                      format.defaultValue = undefined;
-                    }
-                    return generateForm(format);
-                  })}
-                </div>
-                {fields.description !== undefined &&
-                  <p id={fields.id + "-DESCRIPTION"}>{fields.description}</p>}
-              </div>
-            );
-          } else {
-            // The field is not included in the list
-            return null;
-          }
+          return generateInputFormat(fields);
         } else {
           // There was an error constructing the input group
           return null;
@@ -164,6 +79,101 @@ export default function Form({schema, uiSchema, externalData, schemaID, includeF
       }
     } else {
       // In the case where fields is empty, return null
+      return null;
+    }
+  };
+
+  // Generates an explicit list of input fields for an InputGroup
+  const generateInputList = (fields) => {
+    // The specific list of items has been provided
+    let hasVisibleChild = false;
+    let itemGroup = (
+      <div id={fields.id} key={fields.id}>
+        {fields.label !== undefined &&
+          <h2 id={fields.id + "-LABEL"}>{fields.label}</h2>}
+        <div id={fields.id+"-INPUTS"}>
+          {fields.items.map((field, i) => {
+            // If initial values have been passed in with the form data, overwrite the field object
+            if (formData[fields.id] !== undefined &&
+              Array.isArray(formData[fields.id]) && i < formData[fields.id].length) {
+              field.defaultValue = formData[fields.id][i];
+            }
+            // If the input group is of type array, add the array index for update handling
+            if (fields.groupType === "array") {
+              field.arrayIndex = i;
+            }
+            
+            // If a child is visible, indicate that the group should be shown
+            let child = generateForm(field);
+            if (child !== null) {
+              hasVisibleChild = true;
+            }
+
+            return child;
+          })}
+        </div>
+        {fields.description !== undefined &&
+          <p id={fields.id + "-DESCRIPTION"}>{fields.description}</p>}
+      </div>
+    );
+
+    // Only show the input group if it has a child to display
+    if (hasVisibleChild) {
+      return itemGroup;
+    } else {
+      return null;
+    }
+  };
+
+  // Generates a list of input fields of the correct length for a generic InputGroup array
+  const generateInputFormat = (fields) => {
+    // A generic item format has been provided, so compute the number of elements that
+    // should be rendered
+    let listLength = 1;
+    let valueCount = 0;
+    let defaultsCount = 0;
+
+    // If the list has a min length set that to the current list length
+    if (fields.minItems !== undefined && fields.minItems > 0) {
+      listLength = fields.minItems;
+    }
+    // If more values have been passed in with the form data than the min length, update the length 
+    if (formData[fields.id] !== undefined && Array.isArray(formData[fields.id])) {
+      valueCount = formData[fields.id].length;
+      listLength = Math.max(listLength, valueCount);
+    }
+    // If there are more default values than the current list length, 
+    if (fields.defaultValue !== undefined && fields.defaultValue.length > 0) {
+      defaultsCount = fields.defaultValue.length;
+      listLength = Math.max(listLength, defaultsCount);
+    }
+    
+    if (includeFields.includes(fields.id)) {
+      return (
+        <div id={fields.id} key={fields.id}>
+          {fields.label !== undefined &&
+            <h2 id={fields.id + "-LABEL"}>{fields.label}</h2>}
+          <div id={fields.id+"-INPUTS"}>
+            {[...Array(listLength)].map((_, i) => {
+              // For each element within the computed list length, check whether it has an initial value
+              let format = fields.itemFormat;
+              format.arrayIndex = i;
+              if (i < valueCount) {
+                format.defaultValue = formData[fields.id][i];
+              } else if (i < defaultsCount) {
+                format.defaultValue = fields.defaultValue[i];
+              } else {
+                format.defaultValue = undefined;
+              }
+              return generateForm(format);
+            })}
+          </div>
+          {fields.description !== undefined &&
+            <p id={fields.id + "-DESCRIPTION"}>{fields.description}</p>}
+        </div>
+      );
+    } else {
+      // The field is not included in the list
       return null;
     }
   };
