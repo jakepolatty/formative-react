17b99091e7b36bb28107b5db33994cd96489d275
Jake Polatty <jakepolatty@gmail.com>
2019-06-27 05:58:57 -0700

    Added multi-page navigation demo

--- src/App.js
+++ src/App.js
@@ -1,56 +1,95 @@
 import React, {useState} from 'react';
 import FormPage from './components/FormPage/FormPage';
+import Button from 'react-bootstrap/Button';
 // These will be local to the project
 import sampleUISchema from './sample/sampleUISchema.js';
 
 function App() {
   const [currentPage, setCurrentPage] = useState(0);
+  const pages = [
+    (<FormPage
+      index={0}
+      schemaEndpoint="https://comt.s3.amazonaws.com/schema/"
+      schemas={{
+        "wms-a": {
+          "schema": "wms",
+          "include": ["layer", "style"]
+        },
+        "comt": {
+          "schema": "comt",
+          "include": ["project", "subproject", "model"]
+        },
+      }}
+      dataApiEndpoint="http://localhost:8888/api/"
+      uiSchema={sampleUISchema}
+    />),
+    (<FormPage
+      index={1}
+      schemaEndpoint="https://comt.s3.amazonaws.com/schema/"
+      schemas={{
+        "wms-b": {
+          "schema": "wms",
+          "include": ["opacity", "transparent", "elevation"]
+        },
+      }}
+      dataApiEndpoint="http://localhost:8888/api/"
+      uiSchema={sampleUISchema}
+    />),
+    (<FormPage
+      index={2}
+      schemaEndpoint="https://comt.s3.amazonaws.com/schema/"
+      schemas={{
+        "comt": {
+          "schema": "comt",
+          "include": ["subproject", "model"]
+        },
+      }}
+      dataApiEndpoint="http://localhost:8888/api/"
+      uiSchema={sampleUISchema}
+    />),
+    (<FormPage
+      index={3}
+      schemaEndpoint="https://comt.s3.amazonaws.com/schema/"
+      schemas={{
+        "wms": {
+          "schema": "wms",
+          "include": ["layer", "style", "opacity", "transparent", "elevation"]
+        },
+      }}
+      dataApiEndpoint="http://localhost:8888/api/"
+      uiSchema={sampleUISchema}
+    />),
+  ];
 
-  const switchPage = () => {
-    if (currentPage === 0) {
-      setCurrentPage(1);
-    } else {
-      setCurrentPage(0);
+  const prevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  const nextPage = () => {
+    if (currentPage < pages.length - 1) {
+      setCurrentPage(currentPage + 1);
     }
   }
 
   return (
     <div className="App" margin="20px">
-      {(currentPage === 0) ? 
-        (<FormPage
-          index={0}
-          schemaEndpoint="https://comt.s3.amazonaws.com/schema/"
-          schemas={{
-            "wms-a": {
-              "schema": "wms",
-              "include": ["layer", "style"]
-            },
-            "comt": {
-              "schema": "comt",
-              "include": ["project", "subproject", "model"]
-            },
-          }}
-          dataApiEndpoint="http://localhost:8888/api/"
-          uiSchema={sampleUISchema}
-        />) :
-        (<FormPage
-          index={1}
-          schemaEndpoint="https://comt.s3.amazonaws.com/schema/"
-          schemas={{
-            "wms-b": {
-              "schema": "wms",
-              "include": ["opacity", "transparent", "elevation"]
-            },
-          }}
-          dataApiEndpoint="http://localhost:8888/api/"
-          uiSchema={sampleUISchema}
-        />)
-      }
-      <button
-        onClick={() => switchPage()}
+      {pages[currentPage]}
+      <Button
+        onClick={() => prevPage()}
+        disabled={currentPage === 0}
+        variant={currentPage === 0 ? "light" : "primary"}
       >
-        Other Page
-      </button>
+        Previous Page
+      </Button>
+      <Button
+        onClick={() => nextPage()}
+        disabled={currentPage === pages.length - 1}
+        variant={currentPage === pages.length - 1 ? "light" : "primary"}
+      >
+        Next Page
+      </Button>
     </div>
   );
 }
