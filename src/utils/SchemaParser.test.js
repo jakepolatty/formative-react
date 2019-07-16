3dcb4b12850dd2120b54962f14c9b92eede68d3b
Jake Polatty <jakepolatty@gmail.com>
2019-07-16 14:48:48 -0400

    Added sample schemas for all data types for SchemaParser testing

--- src/utils/SchemaParser.test.js
+++ src/utils/SchemaParser.test.js
@@ -1,5 +1,115 @@
 import SchemaParser from './SchemaParser';
 
+const stringSchema = {
+  title: "Description",
+  description: "Human-readable description of the distribution",
+  type: "string",
+  minLength: 1
+};
+
+const stringEnumSchema = {
+  title: "Public Access Level",
+  description: "This may include information regarding access or restrictions",
+  enum: [
+    "public",
+    "restricted public",
+    "non-public"
+  ]
+};
+
+const numberSchema = {
+  title: "Elevation",
+  description: "The elevation of a layer in meters",
+  type: "number"
+};
+
+const integerSchema = {
+  title: "Opacity",
+  description: "The percentage opacity of the layer",
+  type: "integer"
+};
+
+const booleanSchema = {
+  title: "Data Quality",
+  description:  "Whether the dataset meets the agency’s Information Quality Guidelines (true/false).",
+  type: "boolean"
+};
+
+const arrayFormatSchema = {
+  title: "Tags",
+  description:  "Tags (or keywords) help users discover your dataset",
+  type: "array",
+  items: {
+    "type": "string",
+    "minLength": 1
+  },
+  minItems: 1
+};
+
+const objectSchema = {
+  title: "WMS",
+  description: "A simple form for WMS specification.",
+  type: "object",
+  required: [
+    "layer",
+    "style"
+  ],
+  properties: {
+    layer: {
+      type: "string",
+      title: "LAYER",
+      enum: [
+        "LayerA",
+        "LayerB",
+        "LayerC",
+        "TODO_populate_this_enum_with_endpoint"
+      ]
+    },
+    style: {
+      type: "string",
+      title: "STYLE",
+      enum: [
+        "StyleA",
+        "StyleB",
+        "StyleC",
+        "TODO_populate_this_enum_with_endpoint"
+      ]
+    },
+    opacity: {
+      type: "integer",
+      title: "Opacity"
+    },
+    transparent: {
+      type: "boolean",
+      title: "Transparent"
+    },
+    elevation: {
+      type: "integer",
+      title: "Elevation",
+      enum: [
+        1,
+        2,
+        3,
+        4
+      ]
+    }
+  }
+};
+
+const anyOfSchema = {
+  title: "Spatial",
+  description: "The range of spatial applicability of a dataset. Could include a spatial region like a bounding box or a named place.",
+  anyOf: [
+    {
+      type: "string",
+      minLength: 1
+    },
+    {
+      type: "null"
+    }
+  ]
+};
+
 describe("SchemaParser", () => {
   it("should return null for an empty schema", () => {
     let parsed1 = SchemaParser.convertSchemaLayer(null, "root", {});
