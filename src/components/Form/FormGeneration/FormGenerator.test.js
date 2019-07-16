18493e9644dc0c3e139765ba39e5285011263dcc
Jake Polatty <jakepolatty@gmail.com>
2019-07-16 12:02:30 -0400

    Wrote out all FormGenerator unit tests for single input fields

--- src/components/Form/FormGeneration/FormGenerator.test.js
+++ src/components/Form/FormGeneration/FormGenerator.test.js
@@ -4,13 +4,15 @@ import FormGenerator from './FormGenerator';
 import Input from '../../inputs/Input/Input';
 import TextInput from '../../inputs/TextInput/TextInput';
 import SelectInput from '../../inputs/SelectInput/SelectInput';
+import CheckboxInput from '../../inputs/CheckboxInput/CheckboxInput';
 
 const textField = {
   "type": "TextInput",
   "id": "rights",
   "label": "Rights",
   "description": "This may include information regarding access or restrictions.",
-  "info": "This should also provide an explanation for the selected \"accessLevel\". Text, 255 characters."
+  "info": "This should also provide an explanation for the selected \"accessLevel\". Text, 255 characters.",
+  "defaultValue": "private"
 };
 
 const selectField = {
@@ -23,7 +25,48 @@ const selectField = {
     "restricted public",
     "non-public"
   ],
-  "required": true
+  "required": true,
+};
+
+const checkboxField = {
+  "type": "CheckboxInput",
+  "id": "dataQuality",
+  "label": "Data Quality",
+  "description": "Whether the dataset meets the agency’s Information Quality Guidelines (true/false)."
+};
+
+const nonexistentField = {
+  "type": "NotAnInput",
+  "id": "doesNotExist",
+  "label": "Nothing",
+  "description": "void"
+};
+
+const inputList = {
+  "type": "InputGroup",
+  "id": "dataset",
+  "label": "Project Open Data Dataset",
+  "groupType": "object",
+  "description": "The metadata format for all federal open data.",
+  "items": {
+    textField,
+    selectField,
+    CheckboxInput
+  }
+};
+
+const inputFormatGroup = {
+  "type": "InputGroup",
+  "id": "references",
+  "itemFormat": {
+    "type": "TextInput",
+    "id": "references"
+  },
+  "minItems": 1,
+  "unique": true,
+  "groupType": "array",
+  "label": "Related Documents",
+  "description": "Related documents such as technical information about a dataset, developer documentation, etc."
 };
 
 describe("FormGenerator", () => {
@@ -44,16 +87,61 @@ describe("FormGenerator", () => {
   });
 
   it("should generate single fields from an input", () => {
-    let generator = new FormGenerator({}, () => null, [], () => null);
-    let field1 = generator.generateForm
+    let generator = new FormGenerator({}, () => null,
+      ["rights", "accessLevel", "dataQuality"], () => null);
+
+    let field1 = mount(generator.generateForm(textField));
+    let input1 = field1.find(TextInput);
+    expect(input1.length).toEqual(1);
+    expect(input1.prop("id")).toEqual("rights");
+    expect(input1.prop("initialValue")).toEqual("private");
+
+    let field2 = mount(generator.generateForm(selectField));
+    expect(field2.prop("label")).toEqual("Public Access Level");
+    expect(field2.prop("description")).toEqual("The degree to which this dataset could be made publicly-available");
+    let input2 = field2.find(SelectInput);
+    expect(input2.length).toEqual(1);
+    expect(input2.prop("id")).toEqual("accessLevel");
+    expect(input2.prop("options")).toEqual(["public", "restricted public", "non-public"]);
+
+    let field3 = mount(generator.generateForm(checkboxField));
+    let input3 = field3.find(CheckboxInput);
+    expect(input3.length).toEqual(1);
+    expect(input3.prop("id")).toEqual("dataQuality");
   });
 
   it("should skip single fields that are not in the include list", () => {
+    let generator = new FormGenerator({}, () => null, [], () => null);
 
+    let field1 = generator.generateForm(textField);
+    expect(field1).toBeNull();
+
+    let field2 = generator.generateForm(selectField);
+    expect(field2).toBeNull();
+
+    let field3 = generator.generateForm(checkboxField);
+    expect(field3).toBeNull();
+  });
+
+  it("should return null for fields with unavailable types", () => {
+    let generator = new FormGenerator({}, () => null, ["nonexistentField"], () => null);
+
+    let field = generator.generateForm(nonexistentField);
+    expect(field).toBeNull();
   });
 
   it("should merge in prepopulated form data when it exists", () => {
+    let generator = new FormGenerator({"rights": "public", "accessLevel": "public", "dataQuality": true},
+      () => null, ["rights", "accessLevel", "dataQuality"], () => {});
 
+    let field1 = shallow(generator.generateForm(textField));
+    expect(field1.prop("initialValue")).toEqual("public");
+
+    let field2 = shallow(generator.generateForm(selectField));
+    expect(field2.prop("initialValue")).toEqual("public");
+
+    let field3 = shallow(generator.generateForm(checkboxField));
+    expect(field3.prop("initialValue")).toEqual(true);
   });
 
   it("should generate lists of fields for a specific input group", () => {
